c023fa044e053ccf0256f3bbe6deaeb360d4592b
Matt Hampton <matthew.j.hampton@gmail.com>
2022-01-05 13:31:24 +0200

    Configure some entry points

--- setup.py
+++ setup.py
@@ -3,4 +3,10 @@ import setuptools
 setuptools.setup(
     name="beangrow",
     packages=setuptools.find_packages(exclude=["beangrow_tests"]),
+    entry_points = {
+        'console_scripts': [
+            'beangrow-configure=beangrow.configure:main',
+            'beangrow-returns=beangrow.compute_returns:main',
+        ],
+    }
 )
